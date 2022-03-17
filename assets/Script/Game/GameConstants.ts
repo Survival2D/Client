6f229c8702a07b5137d9a85127082e8074a25d02
quantm0201 <quantm0201@gmail.com>
2022-03-17 22:18:55 +0700

    refactor player

--- assets/Script/Game/GameConstants.ts
+++ assets/Script/Game/GameConstants.ts
@@ -1,3 +1,23 @@
 export class PlayerColor {
-    static basic: cc.Color = cc.color().fromHEX("#f8c574");
+    static body : cc.Color[] = [
+        cc.color().fromHEX("#f8c574"),
+        cc.color().fromHEX("#c40000"),
+        cc.color().fromHEX("#bc002d"),
+        cc.color().fromHEX("#1b400c"),
+        cc.color().fromHEX("#990000"),
+    ]
+    static hand : cc.Color[] = [
+        cc.color().fromHEX("#f8c574"),
+        cc.color().fromHEX("#16b900"),
+        cc.color().fromHEX("#FFFFFF"),
+        cc.color().fromHEX("#b5c58b"),
+        cc.color().fromHEX("#4c1111"),
+    ]
+    static back : cc.Color[] = [
+        cc.color().fromHEX("#816537"),
+        cc.color().fromHEX("#059300"),
+        cc.color().fromHEX("#c0a73f"),
+        cc.color().fromHEX("#ab7c29"),
+        cc.color().fromHEX("#ffcc00"),
+    ]
 }
--- assets/Script/Game/Player.ts
+++ assets/Script/Game/Player.ts
@@ -12,15 +12,6 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class Player extends cc.Component {
 
-    isUp: boolean = false;
-    isDown: boolean = false;
-    isLeft: boolean = false;
-    isRight: boolean = false;
-
-
-    @property
-    vel: number = 200;
-
     @property(cc.Node)
     body: cc.Node = null;
 
@@ -31,9 +22,11 @@ export default class Player extends cc.Component {
     rightHand: cc.Node = null;
 
     @property(cc.Node)
-    camera: cc.Node = null;
+    backPack: cc.Node = null;
 
-    private color: cc.Color = PlayerColor.basic;
+    private bodyColor: cc.Color = PlayerColor.body[1];
+    private handColor: cc.Color = PlayerColor.hand[1];
+    private backColor: cc.Color = PlayerColor.back[1];
 
     // LIFE-CYCLE CALLBACKS:
 
@@ -43,103 +36,37 @@ export default class Player extends cc.Component {
 
     genPlayer () {
         let ctx = this.body.getComponent(cc.Graphics);
-        ctx.fillColor = this.color;
+        ctx.fillColor = this.bodyColor;
         ctx.circle(0, 0, 28);
         ctx.fill();
         ctx.stroke();
 
         ctx = this.leftHand.getComponent(cc.Graphics);
-        ctx.fillColor = this.color;
+        ctx.fillColor = this.handColor;
         ctx.circle(0, 0, 10);
         ctx.fill();
         ctx.stroke();
 
         ctx = this.rightHand.getComponent(cc.Graphics);
-        ctx.fillColor = this.color;
+        ctx.fillColor = this.handColor;
         ctx.circle(0, 0, 10);
+        ctx.fill();
+        ctx.stroke();
+
+        ctx = this.backPack.getComponent(cc.Graphics);
+        ctx.fillColor = this.backColor;
+        ctx.circle(0, 0, 27);
         ctx.fill();
         ctx.stroke();
 
         this.body.setPosition(0, 0);
         this.leftHand.setPosition(-25, 25);
         this.rightHand.setPosition(25, 25);
+        this.backPack.setPosition(0, -10);
     }
 
     start () {
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
-        this.camera.on(cc.Node.EventType.MOUSE_MOVE, this.onMouseMove, this);
-    }
 
-    onKeyDown (event) {
-        switch (event.keyCode) {
-            case cc.macro.KEY.a:
-                this.isLeft = true;
-                break;
-            case cc.macro.KEY.s:
-                this.isDown = true;
-                break;
-            case cc.macro.KEY.d:
-                this.isRight = true;
-                break;
-            case cc.macro.KEY.w:
-                this.isUp = true;
-                break;
-        }
-    }
-
-    onKeyUp (event) {
-        switch (event.keyCode) {
-            case cc.macro.KEY.a:
-                this.isLeft = false;
-                break;
-            case cc.macro.KEY.s:
-                this.isDown = false;
-                break;
-            case cc.macro.KEY.d:
-                this.isRight = false;
-                break;
-            case cc.macro.KEY.w:
-                this.isUp = false;
-                break;
-        }
-    }
-
-    onMouseMove (event) {
-        let dx = event.getLocationX() - this.camera.width/2;
-        let dy = event.getLocationY() - this.camera.height/2;
-        let angle = Math.atan(-dx/dy) * 180 / Math.PI;
-        if (dy < 0) angle = 180 + angle;
-        this.node.angle = angle;
-    }
-
-    update (dt) {
-        if (this.isLeft && this.isUp) {
-            this.node.x -= this.vel/1.4 * dt;
-            this.node.y += this.vel/1.4 * dt;
-        }
-        else if (this.isLeft && this.isDown) {
-            this.node.x -= this.vel/1.4 * dt;
-            this.node.y -= this.vel/1.4 * dt;
-        }
-        else if (this.isRight && this.isUp) {
-            this.node.x += this.vel/1.4 * dt;
-            this.node.y += this.vel/1.4 * dt;
-        }
-        else if (this.isRight && this.isDown) {
-            this.node.x += this.vel/1.4 * dt;
-            this.node.y -= this.vel/1.4 * dt;
-        }
-        else if (this.isLeft && this.isRight) {}
-        else if (this.isUp && this.isDown) {}
-        else if (this.isLeft) this.node.x -= this.vel * dt;
-        else if (this.isRight) this.node.x += this.vel * dt;
-        else if (this.isUp) this.node.y += this.vel * dt;
-        else if (this.isDown) this.node.y -= this.vel * dt;
-
-        // move camera following player
-        this.camera.x = this.node.x;
-        this.camera.y = this.node.y;
     }
 
     onDestroy () {
