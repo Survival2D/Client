707d77a9bedbb11ecb3c350c3ed39d24e9c57339
TienNM99 <tiennm99@outlook.com>
2022-08-01 23:18:10 +0700

    [Add] test babel

--- src/test.js
+++ src/test.js
@@ -3,3 +3,8 @@ setInterval(function () {
     // sendSpinRequest();
     sendTestRequest();
 }, 1000);
+setTimeout(() => {
+    cc.log("Test");
+}, 1000);
+const sum = [1, 2, 3, 4, 5, 6].reduce((a, b) => a + b);
+let included = [1, 2, 3, 4].includes(1);
