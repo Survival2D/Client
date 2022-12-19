581bd53fd585f0a4448db7a2baa2ecc800d789dc
quantm0201 <quantm0201@gmail.com>
2022-12-19 22:20:47 +0700

    enable zoom test

--- src/Constant.js
+++ src/Constant.js
@@ -11,7 +11,7 @@ Constant.LOGIC_VIEW_WIDTH = 2400;
 Constant.LOGIC_VIEW_HEIGHT = 1200;
 
 Constant.IS_OFFLINE = false;
-Constant.TEST = false;
+Constant.TEST = true;
 
 Constant.PLAYER_COLOR = [
     cc.color("#f8c574"),
