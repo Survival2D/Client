86a75bc2b98add1fac7745a88477ba155771684a
quantm0201 <quantm0201@gmail.com>
2023-05-27 20:03:45 +0700

    [feat] send set auto play

--- src/Game/SettingGUI.js
+++ src/Game/SettingGUI.js
@@ -36,7 +36,12 @@ const SettingGUI = BaseLayer.extend({
         Config.ENABLE_AUTO_PLAY = !Config.ENABLE_AUTO_PLAY;
         this.lblEnableAutoPlay.setString(Config.ENABLE_AUTO_PLAY ? "Enabled" : "Disabled");
 
-        //TODO: setAutoPlay
+        let builder = new flatbuffers.Builder(0);
+        let setAutoPlayRequest = fbs.PlayerMoveRequest.createSetAutoPlayRequest(builder, Config.ENABLE_AUTO_PLAY);
+        let request = fbs.Request.createRequest(builder,
+            fbs.RequestUnion.SetAutoPlayRequest, setAutoPlayRequest);
+        builder.finish(request);
+        fbsClient.sendBinary(builder.asUint8Array());
     }
 });
 
